ddbd5df9919251844eb959fd084491650f1e18bb
mpdwyer <mpdwyer93@gmail.com>
2015-10-16 23:24:52 -0400

    Preliminary Navigation Scene
    Director Script not implemented. ClickToMove Script prototype moves all
    the agents collectively instead of just the one selected. Should be
    fixed when director is implemented. NavMeshObstacles not yet
    implemented. Stage is very basic.

---
+++ BAssignments/B1/B1/Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+
+    public class ClickToMove : MonoBehaviour
+    {
+        private NavMeshAgent navMeshAgent;
+        private bool walking;
+        private bool agentClicked;
+        private bool obstacleClicked;
+        // Use this for initialization
+        void Awake()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Input.GetMouseButton(0))
+            {
+                if(Physics.Raycast(ray, out hit, 100))
+                {
+                    if (hit.collider.CompareTag("Agent"))
+                    {
+                        agentClicked = true;
+                    }
+                    else if (hit.collider.CompareTag("Obstacle"))
+                    {
+                        obstacleClicked = true;
+                    }
+                    else
+                    {
+                        if (agentClicked)
+                        {
+                            walking = true;
+                            agentClicked = false;
+                            navMeshAgent.destination = hit.point;
+                            navMeshAgent.Resume();
+
+                        }
+ 
+                    }
+                }
+            }
+
+        }
+    }
+}
