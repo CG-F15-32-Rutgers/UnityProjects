d7a1331a4442bbcfec097b426fa107444f41bc72
Le Liu <leliu08640@gmail.com>
2015-11-23 20:32:18 -0500

    testing some interaction

--- BAssignments/B2/Assets/MyBehaviorTree.cs
+++ BAssignments/B2/Assets/MyBehaviorTree.cs
@@ -8,6 +8,7 @@ public class MyBehaviorTree : MonoBehaviour
 	public Transform wander2;
 	public Transform wander3;
 	public GameObject participant;
+	public GameObject participant2;
 
 	private BehaviorAgent behaviorAgent;
 	// Use this for initialization
@@ -24,6 +25,30 @@ public class MyBehaviorTree : MonoBehaviour
 	
 	}
 
+	protected Node GunPoint()
+	{
+		return
+			new DecoratorPrintResult(
+				new SequenceParallel( 
+			             participant.GetComponent<BehaviorMecanim>().ST_PlayGesture("PistolAim", AnimationLayer.Hand, 6000),
+			             new Sequence(						 
+	                     participant2.GetComponent<BehaviorMecanim>().ST_PlayGesture("Shock", AnimationLayer.Hand, 1000),
+						 participant2.GetComponent<BehaviorMecanim>().ST_PlayGesture("HandsUp", AnimationLayer.Hand, 5000))));
+	}
+
+	
+	public Node ST_ApproachAndOrient()
+	{
+		Val <Vector3> p1pos = Val.V(() => participant.gameObject.transform.position);
+		Val<Vector3> p2pos = Val.V(() => participant2.gameObject.transform.position);
+		return new Sequence(
+			new SequenceParallel(
+			participant.GetComponent<BehaviorMecanim>().Node_OrientTowards(p2pos),
+			participant2.GetComponent<BehaviorMecanim>().Node_OrientTowards(p1pos)));
+	}
+
+
+
 	protected Node ST_ApproachAndWait(Transform target)
 	{
 		Val<Vector3> position = Val.V (() => target.position);
@@ -33,10 +58,12 @@ public class MyBehaviorTree : MonoBehaviour
 	protected Node BuildTreeRoot()
 	{
 		return
-			new DecoratorLoop(
-				new SequenceShuffle(
-					this.ST_ApproachAndWait(this.wander1),
-					this.ST_ApproachAndWait(this.wander2),
-					this.ST_ApproachAndWait(this.wander3)));
+			new DecoratorLoop (
+				new SequenceParallel (
+					new Sequence (
+					this.ST_ApproachAndOrient(),
+					this.GunPoint ())
+		)
+		);
 	}
 }
