26a0604fb8f72221ff0e5fe7a8c2747e2c445fcf
mpdwyer <mpdwyer93@gmail.com>
2015-12-13 14:58:40 -0500

    Starting B3

---
+++ BAssignments/B3/Assets/Core/Scripts/Controllers/Director.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+
+
+    public class Director : MonoBehaviour
+    {
+
+       // private bool agentClicked;
+        private bool obstacleClicked;
+        private float speed = 2.0f;
+        private GameObject obstacle;
+
+        //private ClickToMove script;
+        //private GameObject[] agents;
+
+        void start()
+        {
+            //agentClicked = false;
+            obstacleClicked = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Input.GetMouseButton(0))
+            {
+                if (Physics.Raycast(ray, out hit, 100))
+                {
+                    /*if (hit.collider.CompareTag("Agent"))
+                    {
+                        agentClicked = true;
+                        obstacleClicked = false;
+                        script = GameObject.Find(hit.collider.name).GetComponent<ClickToMove>();
+                        script.selected = true;
+                    }*/
+                    //else if
+                    if (hit.collider.CompareTag("Obstacle"))
+                    {
+                        if(obstacle != null)
+                        {
+                            obstacle.GetComponent<Renderer>().material.color = Color.black;
+                        }
+  
+                        obstacleClicked = true;
+                        obstacle = GameObject.Find(hit.collider.name);
+                        obstacle.GetComponent<Renderer>().material.color = Color.red;
+                    }
+                    else
+                    {
+                        obstacleClicked = false;
+                        obstacle.GetComponent<Renderer>().material.color = Color.black;
+                        /*if (agentClicked)
+                        {
+                            agentClicked = false;
+                            agents = GameObject.FindGameObjectsWithTag("Agent");
+                            foreach (GameObject go in agents)
+                            {
+                                script = go.GetComponent<ClickToMove>();
+                                if(script.selected == true)
+                                {
+                                    script.selected = false;
+                                    script.moveAgent(hit, true);
+                                }
+
+                            }
+                        }*/
+
+                    }
+                }
+            }
+
+            if (obstacleClicked)
+            {
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    obstacle.transform.position += Vector3.right * speed * Time.deltaTime;
+                }
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    obstacle.transform.position += Vector3.left * speed * Time.deltaTime;
+                }
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    obstacle.transform.position += Vector3.forward * speed * Time.deltaTime;
+                }
+                if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    obstacle.transform.position += Vector3.back * speed * Time.deltaTime;
+                }
+            }
+
+        }
+    }
+}
