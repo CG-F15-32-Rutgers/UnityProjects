3337440df5de443dce947065b93e9c86d1a0b7de
mpdwyer <mpdwyer93@gmail.com>
2015-12-18 16:50:50 -0500

    started interactive part

---
+++ BAssignments/B3/Assets/Scripts/KyleControlScript.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KyleControlScript : MonoBehaviour
+{
+
+    public float animSpeed = 1.5f;
+    public float lookSmoother = 3f;
+    public float tick;
+    public UnityEngine.UI.Text hint;
+
+    private Animator anim; // a reference to the animator on the character
+    private GameObject dismissableNPC;
+
+    void Start()
+    {
+        // initialising reference variables
+        anim = GetComponent<Animator>();
+        if (anim.layerCount == 2)
+        {
+            anim.SetLayerWeight(1, 1);
+        }
+        tick = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Script from our B1 modified VERY slightly for this assignment (no jump / fall)
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (v > 0 && Input.GetKey(KeyCode.LeftShift))
+        {
+            v = v * 6.5f;
+        }
+        else if (v < 0)
+        {
+            v = v * 0.5f;
+            h = h * -1;
+        }
+
+        if (v != 0)
+        {
+            Quaternion rotation = Quaternion.Euler(new Vector3(0, h * 20, 0));
+            transform.rotation = transform.rotation * rotation;
+        }
+        else if (v == 0)
+        {
+            h = h * 75;
+        }
+
+        anim.SetFloat("Speed", v);
+        anim.SetFloat("Direction", h);
+        anim.speed = animSpeed;
+
+        /*if (Input.GetKeyDown(KeyCode.Space) && dismissableNPC != null)
+        {
+            GetComponent<SpeechBubbleController>().DisplaySpeechBubble("Sir, I'm going to have to ask you to leave.");
+            dismissableNPC.GetComponent<ShopperMeta>().StillActive = false;
+        }*/
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Shopper") || other.gameObject.CompareTag("Thief"))
+        {
+            dismissableNPC = other.gameObject;
+            hint.gameObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.Equals(dismissableNPC))
+        {
+            dismissableNPC = null;
+            hint.gameObject.SetActive(false);
+        }
+    }
+
+
+}
