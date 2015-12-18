bfb85584dce3b3927aadde2e5b4d1b2848c7095c
Le Liu <leliu08640@gmail.com>
2015-12-18 16:53:46 -0500

    zombie damage

---
+++ BAssignments/B3/Assets/ZombieDamageCharacter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieDamageCharacter : MonoBehaviour {
+
+	public int health = 100;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(health + "hp left");
+		if(health == 0){
+			Destroy(this);
+		}
+	}
+
+	void OnCollisionEnter(Collision col){
+		Debug.Log("colliding");
+		if(col.gameObject.tag == "Zombie"){
+			health-=5;
+		}
+	}
+}
